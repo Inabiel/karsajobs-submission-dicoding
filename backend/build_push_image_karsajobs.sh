f2c0084e39aae0a47944dc29c8830328d597f0a7
Nabil Izzullah <nabiel.p@students.amikom.ac.id>
2022-12-26 01:05:13 +0700

    update token

--- backend/build_push_image_karsajobs.sh
+++ backend/build_push_image_karsajobs.sh
@@ -1,11 +1,8 @@
 #building the image
 docker build . -t ghcr.io/inabiel/karsajobs:latest
 
-#exporting github personal access token (dont use this, please! :( )
-export CR_PAT=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
-
 #cli login
-echo $CR_PAT | docker login ghcr.io -u inabiel --password-stdin
+echo $GITHUB_TOKEN | docker login ghcr.io -u inabiel --password-stdin
 
 #push to the specified repo
 docker push ghcr.io/inabiel/karsajobs:latest
--- frontend/build_push_image_karsajobs_ui.sh
+++ frontend/build_push_image_karsajobs_ui.sh
@@ -1,11 +1,8 @@
 #building the image
 docker build . -t ghcr.io/inabiel/karsajobs-ui:latest
 
-#exporting github personal access token (dont use this, please! :( )
-export CR_PAT=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
-
 #cli login
-echo $CR_PAT | docker login ghcr.io -u inabiel --password-stdin
+echo $GITHUB_TOKEN | docker login ghcr.io -u inabiel --password-stdin
 
 #push to the specified repo
 docker push ghcr.io/inabiel/karsajobs-ui:latest
